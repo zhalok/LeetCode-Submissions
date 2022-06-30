982e970ab085dd7171c0f4ef27084338e21c017b
Zhalok Rahman <52986893+zhalok@users.noreply.github.com>
2022-06-30 11:03:53 +0600

    Time: 72 ms (82.32%), Space: 42.3 MB (63.41%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.js
+++ 60-permutation-sequence/60-permutation-sequence.js
@@ -6,7 +6,7 @@
 
 var getPermutation = function (n, k) {
   let perm = [];
-  let ans = [];
+  let ans = "";
   let factorials = [1];
 
   for (let i = 1; i <= n; i++) factorials[i] = factorials[i - 1] * i;
@@ -14,19 +14,14 @@ var getPermutation = function (n, k) {
   for (let i = 1; i <= n; i++) {
     perm.push(i);
   }
-  for (let i = 1; i < n; i++) {
-    let curIdx = Math.ceil(k / factorials[perm.length - 1]);
 
-    // k %= factorials[perm.length - 1];
+  for (let i = 1; i <= n; i++) {
+    let curIdx = Math.ceil(k / factorials[perm.length - 1]);
     k -= (curIdx - 1) * factorials[perm.length - 1];
-    ans.push(perm[curIdx - 1]);
-    perm = [];
-    for (let j = 1; j <= n; j++) if (ans.indexOf(j) == -1) perm.push(j);
+    ans += perm[curIdx - 1].toString();
+    perm.splice(curIdx - 1, 1);
   }
-  for (let i = 1; i <= n; i++) if (ans.indexOf(i) == -1) ans.push(i);
-  let _ans = "";
-  for (let i = 0; i < ans.length; i++) _ans += ans[i];
-  return _ans;
+  return ans;
 };
 
 // console.log(getPermutation(3, 2));
