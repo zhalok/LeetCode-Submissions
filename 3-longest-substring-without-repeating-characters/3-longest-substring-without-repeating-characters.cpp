cc576b22762b1659947104a4d96b213cfcb282c0
Zhalok Rahman <52986893+zhalok@users.noreply.github.com>
2022-08-22 21:56:56 +0600

    Time: 2369 ms (5.00%), Space: 256.5 MB (5.00%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cstddef>
+#include <cstring>
+#include <string>
+#include <system_error>
+#include <vector>
+using namespace std;
+using namespace std;
+#define zhalok Zhalok
+#define inf INT_MAX
+#define ll long long int
+#define ull unsigned long long
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define negloop(i, a, b) for (ll i = a; i >= b; i--)
+#define vi vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define pb push_back
+#define be begin()
+#define en end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define ff first
+#define ss second
+#define MOD 1000000007
+#define sz 10001
+#define ub upper_bound
+#define lb lower_bound
+#define all(v) v.begin(), v.end()
+#define eps 1e-8
+#define pi acos(-1.0)
+
+class Solution {
+
+  int cumsum[200][10001];
+
+  bool check(string s, int len) {
+    int i = 1;
+    int j = len;
+    set<char> ss;
+    while (j < s.size()) {
+      bool flag = true;
+      for (int k = 0; k < 200; k++)
+        if (cumsum[k][j] - cumsum[k][i - 1] > 1) {
+          flag = false;
+          break;
+        }
+      if (flag == true)
+        return true;
+
+      i++;
+      j++;
+    }
+    return false;
+  }
+
+public:
+  ll maxx(ll a, ll b) { return (a > b) ? a : b; }
+
+  int lengthOfLongestSubstring(string s) {
+    s = "*" + s;
+    map<char, int> last;
+    ll ans = 0;
+    ll temp_ans = 0;
+    for (int i = 1; i < s.size(); i++) {
+      if (last[s[i]] == 0) {
+        temp_ans++;
+        last[s[i]] = i;
+      } else {
+        i = last[s[i]];
+        last.clear();
+        temp_ans = 0;
+      }
+      ans = maxx(ans, temp_ans);
+    }
+    return ans;
+  }
+};
+// int main() {
+
+//   freopen("input.txt", "r", stdin);
+//   freopen("output.txt", "w", stdout);
+
+//   string s;
+//   cin >> s;
+//   Solution sol;
+//   cout << sol.lengthOfLongestSubstring(s) << endl;
+// }
