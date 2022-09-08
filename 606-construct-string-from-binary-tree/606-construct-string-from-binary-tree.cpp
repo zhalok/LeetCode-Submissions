b16370767432636eaa76a2f3e54eb3125863622d
Zhalok Rahman <52986893+zhalok@users.noreply.github.com>
2022-09-08 10:20:48 +0600

    Time: 28 ms (72.17%), Space: 25.8 MB (60.58%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -1,13 +1,33 @@
+// class Solution {
+// public:
+//     string tree2str(TreeNode* root) {
+// 		string ans = to_string(root->val);
+// 		if (root->left) //left side check
+// 			ans += "(" + tree2str(root->left) + ")";
+// 		if (root->right) { //right side check
+// 			if (!root->left) ans += "()"; //left side not present, but right side present
+// 			ans += "(" + tree2str(root->right) + ")"; 
+// 		}
+// 		return ans;
+// 	}
+// };
+
+
 class Solution {
 public:
-    string tree2str(TreeNode* root) {
-		string ans = to_string(root->val);
-		if (root->left) //left side check
-			ans += "(" + tree2str(root->left) + ")";
-		if (root->right) { //right side check
-			if (!root->left) ans += "()"; //left side not present, but right side present
-			ans += "(" + tree2str(root->right) + ")"; 
-		}
-		return ans;
-	}
+  string tree2str(TreeNode *root) {
+    if (root == NULL)
+      return "";
+    string val;
+    val = to_string(root->val);
+    ;
+    if (root->left == NULL && root->right == NULL)
+      return val;
+      
+    if (root->right == NULL)
+      return val + "(" + tree2str(root->left) + ")";
+    return val + "(" + tree2str(root->left) + ")(" + tree2str(root->right) +
+           ")";
+           
+  }
 };
