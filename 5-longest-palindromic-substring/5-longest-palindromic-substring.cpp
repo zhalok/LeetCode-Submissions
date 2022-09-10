332637e40eb41334d71d003e7d2b3aa610c4758c
Zhalok Rahman <52986893+zhalok@users.noreply.github.com>
2022-09-10 14:50:47 +0600

    Time: 24 ms (80.76%), Space: 7.1 MB (74.52%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,61 +2,52 @@
 #include <cstdio>
 #include <vector>
 using namespace std;
-int find_odd(string s) {
-  s = '*' + s + '+';
-  int idx[s.size() + 2];
-  memset(idx, 0, sizeof(idx));
-  for (int i = 1; i < s.size() - 1; i++) {
-    while (s[i + idx[i]] == s[i - idx[i]])
-      idx[i]++;
-  }
-  int mx = 0;
-  for (int i = 1; i < s.size() - 1; i++)
-    mx = max(mx, 2 * (idx[i] - 1) + 1);
-  return mx;
-}
-
-int find_even(string s) {
-  s = '*' + s + '+';
-  int idx[s.size() + 2];
-  memset(idx, 0, sizeof(idx));
-  for (int i = 1; i < s.size() - 1; i++) {
-    while (s[i + idx[i] + 1] == s[i - idx[i]])
-      idx[i]++;
-  }
-  int mx = 0;
-  for (int i = 1; i < s.size() - 1; i++)
-    mx = max(mx, 2 * (idx[i]));
-  return mx;
-}
-
-bool check_palindrome(string s) {
-  int i = 0;
-  int j = s.size() - 1;
-  while (i <= j) {
-    if (s[i] != s[j])
-      return false;
-    i++;
-    j--;
-  }
-  return true;
-}
 
 class Solution {
 public:
+  bool isPalindrome(string s) {
+    int i = 0;
+    int j = s.size() - 1;
+    while (i <= j) {
+      if (s[i] != s[j])
+        return false;
+      i++;
+      j--;
+    }
+    return true;
+  }
   string longestPalindrome(string s) {
-    int ans = max(find_even(s), find_odd(s));
+    int ans = 0;
+    for (int i = 0; i < s.size(); i++) {
+      int j = i;
+      int k = i;
+
+      while (j >= 0 and k < s.size() and s[j] == s[k]) {
+        ans = max(ans, (k - j + 1));
+        k++;
+        j--;
+      }
+    }
+    for (int i = 0; i < s.size(); i++) {
+      int j = i - 1;
+      int k = i;
+      while (j >= 0 and k < s.size() and s[j] == s[k]) {
+        ans = max(ans, (k - j + 1));
+        j--;
+        k++;
+      }
+    }
+    string _ans;
     int i = 0;
     int j = ans - 1;
-    bool flag = true;
     while (j < s.size()) {
-      string _s = "";
+      string temp_ans;
       for (int k = i; k <= j; k++)
-        _s += s[k];
-      if (check_palindrome(_s))
-        return _s;
-        i++;
-        j++;
+        temp_ans += s[k];
+      if (isPalindrome(temp_ans))
+        return temp_ans;
+      i++;
+      j++;
     }
     return "";
   }
