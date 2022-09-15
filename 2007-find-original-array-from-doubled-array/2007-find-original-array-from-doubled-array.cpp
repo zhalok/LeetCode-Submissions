379713fd4e73c873872eb16c1d6caa22ddc39ba0
Zhalok Rahman <52986893+zhalok@users.noreply.github.com>
2022-09-15 19:14:29 +0600

    Time: 1909 ms (5.07%), Space: 196.4 MB (7.26%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cstddef>
+#include <cstdio>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+  vector<int> findOriginalArray(vector<int> &changed) {
+    sort(changed.begin(), changed.end());
+    vector<int> original;
+    map<int, bool> mp;
+    for (int i = 0; i < changed.size(); i++)
+      mp[i] = true;
+    for (int i = 0; i < changed.size(); i++)
+      if (mp[i]) {
+        int lo = i + 1;
+        int hi = changed.size() - 1;
+        int idx = 200000;
+        while (lo <= hi) {
+          int mid = (lo + hi) / 2;
+          if (changed[mid] == 2 * changed[i]) {
+            if (mp[mid] == true) {
+              idx = min(idx, mid);
+              hi = mid - 1;
+            } else
+              lo = mid + 1;
+          } else if (changed[mid] < 2 * changed[i])
+            lo = mid + 1;
+          else
+            hi = mid - 1;
+        }
+        if (idx != 200000) {
+          original.push_back(changed[i]);
+          mp[i] = false;
+          mp[idx] = false;
+        } else {
+          original.clear();
+          return original;
+        }
+      }
+    return original;
+  }
+};
+// int main() {
+//   freopen("input.txt", "r", stdin);
+//   freopen("output.txt", "w", stdout);
+//   string s;
+//   cin >> s;
+//   int ans = max(find_even(s), find_odd(s));
+//   cout << ans << endl;
+// }
