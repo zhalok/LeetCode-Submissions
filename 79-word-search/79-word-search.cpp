fb1946ab1489ced0fa79a7b7d29624d12b8e3317
Zhalok Rahman <52986893+zhalok@users.noreply.github.com>
2022-10-01 14:26:47 +0600

    Time: 319 ms (86.10%), Space: 8 MB (79.85%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -17,33 +17,31 @@ public:
   int x_moves[10] = {1, -1, 0, 0};
   int y_moves[10] = {0, 0, 1, -1};
   bool ans = false;
-  bool dfs(int idx, int x, int y, vector<vector<char>> &board, string &word) {
-    if (idx == word.size() - 1 && board[x][y] == word[idx])
-      return true;
-    if (board[x][y] != word[idx])
-      return false;
+  void dfs(int idx, int x, int y, vector<vector<char>> &board, string &word) {
     visited[x][y] = true;
+    if (idx == word.size() - 1) {
+      ans = true;
+      return;
+    }
     for (int i = 0; i < 4; i++) {
       int _x = x + x_moves[i];
       int _y = y + y_moves[i];
-
       if (check_boundary(_x, _y, board.size(), board[0].size()) &&
-          visited[_x][_y] == false) {
-        if (dfs(idx + 1, _x, _y, board, word))
-          return true;
+          word[idx + 1] == board[_x][_y] && visited[_x][_y] == false) {
+        dfs(idx + 1, _x, _y, board, word);
       }
     }
     visited[x][y] = false;
-    return false;
   }
   bool exist(vector<vector<char>> &board, string word) {
     ans = false;
     for (int i = 0; i < board.size(); i++)
       for (int j = 0; j < board[i].size(); j++)
-        if (dfs(0, i, j, board, word))
-          return true;
-
-    return false;
+        if (board[i][j] == word[0]) {
+          // memset(visited, false, sizeof(visited));
+          dfs(0, i, j, board, word);
+        }
+    return ans;
   }
 };
 // int main() {
