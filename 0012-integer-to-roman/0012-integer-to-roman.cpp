fc6c68c2da1fb1a52c04ef2d6a5062364507c6ad
Zhalok Rahman <52986893+zhalok@users.noreply.github.com>
2022-10-20 10:50:06 +0600

    Time: 56 ms (5.40%), Space: 14.3 MB (5.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,147 +1,81 @@
-#include<bits/stdc++.h>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <queue>
+#include <vector>
+#define inf 100000
 using namespace std;
-using namespace std;
-#define zhalok Zhalok
-#define inf 1000000
-#define ll long long int  
-#define ull unsigned long long 
-#define loop(i,a,b) for(ll i=a;i<b;i++)
-#define negloop(i,a,b) for(ll i=a;i>=b;i--)
-#define vi vector<int>
-#define vll vector<ll>
-#define vull vector<ull>
-#define pb push_back
-#define be begin()
-#define en end()
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define vpii vector<pii>
-#define vpll vector<pll>
-#define ff first
-#define ss second
-#define MOD 1000000007
-#define sz 10001
-#define ub upper_bound
-#define lb lower_bound
-#define all(v) v.begin(),v.end()
-#define eps 1e-8
-#define pi acos(-1.0)
 
+// struct ListNode {
+//   int val;
+//   ListNode *next;
+//   ListNode() : val(0), next(nullptr) {}
+//   ListNode(int x) : val(x), next(nullptr) {}
+//   ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
 
+// void ListIterator(ListNode *node) {
+//   while (node != NULL) {
+//     cout << node->val << " ";
+//     node = node->next;
+//   }
+// }
 
 class Solution {
-    
 public:
-   
-   
+  string intToRoman(int num) {
+    map<int, string> int_to_roman;
+    int_to_roman[1] = "I";
+    int_to_roman[4] = "IV";
+    int_to_roman[5] = "V";
+    int_to_roman[9] = "IX";
+    int_to_roman[10] = "X";
+    int_to_roman[40] = "XL";
+    int_to_roman[50] = "L";
+    int_to_roman[90] = "XC";
+    int_to_roman[100] = "C";
+    int_to_roman[400] = "CD";
+    int_to_roman[500] = "D";
+    int_to_roman[900] = "CM";
+    int_to_roman[1000] = "M";
+    vector<int> vals;
+    int init = 1;
+    int idx = 1;
+    int pw = 1;
+    for (auto x : int_to_roman) {
+      vals.push_back(x.first);
+    }
 
-    vi v;
-    map<int,char>mp;
-
-    string convert(int n)
-    {
-
-      string ans;
-
-      for(int i=0;i<v.size();i++)
-      if(v[i]==n)
-      {
-        ans+=mp[v[i]];
-        return ans;
-      }
-
-      for(int i=0;i<v.size();i++)
-      for(int j=i+1;j<v.size();j++)
-      if(v[j]-v[i]==n){
-      
-      ans+=mp[v[i]];
-      ans+=mp[v[j]];
-      
-      return ans;
-      }
-
-      int num = n;
-      while(num)
-      {
-        for(int i=v.size()-1;i>=0;i--)
-        if(v[i]<=num)
-        {
-          num-=v[i];
-          ans+=mp[v[i]];
+    string ans;
+    // sort(vals.begin(), vals.end());
+    reverse(vals.begin(), vals.end());
+    while (num) {
+      // for (auto x : int_to_roman) {
+      //   if (x.first <= num) {
+      //     num -= x.first;
+      //     ans += x.second;
+      //   }
+      // }
+      for (int i = 0; i < vals.size(); i++) {
+        if (vals[i] <= num) {
+          num -= vals[i];
+          ans += int_to_roman[vals[i]];
           break;
         }
-        
       }
-
-      return ans;
-
     }
-
-    vi get_the_vector(int n)
-    {
-      int fact=1;
-      vi v;
-      while(n)
-      {
-        int temp=n%10;
-        v.push_back(temp*fact);
-        n/=10;
-        fact*=10;
-      }
-      reverse(all(v));
-      return v;
-
-    }
-
-    string intToRoman(int num) {
-        
-        mp[1]='I';
-        mp[5]='V';
-        mp[10]='X';
-        mp[50]='L';
-        mp[100]='C';
-        mp[500]='D';
-        mp[1000]='M';
-       
-        v.push_back(1);
-        v.push_back(5);
-        v.push_back(10);
-        v.push_back(50);
-        v.push_back(100);
-        v.push_back(500);
-        v.push_back(1000);
-
-        vi vv=get_the_vector(num);
-        for(auto x:vv) cout<<x<<" ";
-        cout<<endl;
-
-        string ans;
-        for(int i=0;i<vv.size();i++)
-        ans+=convert(vv[i]);
-
-        return ans;
-
-        
-
-       
-    }
-
-     
-      
-  
+    return ans;
+  }
 };
 
-// int main()
-// {
-//     freopen("input.txt","r",stdin);
-//     freopen("output.txt","w",stdout);
-//     Solution solution;
-//     int n;
-//     cin>>n;
-//     cout<<solution.intToRoman(n)<<endl;
-
-
-    
-    
-
+// int main() {
+//   freopen("input.txt", "r", stdin);
+//   freopen("output.txt", "w", stdout);
+//   int num;
+//   cin >> num;
+//   Solution sol;
+//   cout << sol.intToRoman(num) << endl;
 // }
